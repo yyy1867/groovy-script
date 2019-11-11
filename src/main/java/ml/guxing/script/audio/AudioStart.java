37e6c2a32dae75155239e22a26844b3b5d6e234c
29582146@qq.com <aaa258123>
2019-11-11 13:49:20 +0800

    装机备份恢复部分笔记

--- src/main/java/ml/guxing/script/audio/AudioStart.java
+++ src/main/java/ml/guxing/script/audio/AudioStart.java
@@ -5,21 +5,32 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class AudioStart implements Runnable {
 
     private static boolean status = true;
+    private static boolean isPlay = true;
     private static TargetDataLine targetDataLine;
+    private static SourceDataLine sourceDataLine;
     private static ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(102400);
-    private static AudioFormat audio = new AudioFormat(8000f, 16, 2, true, false);
-    private static Stack<byte[]> caches = new Stack();
+    private static AudioFormat audio = new AudioFormat(8000f, 16
+            , 1, true, false);
+
+    static {
+//        audio = new AudioFormat(AudioFormat.Encoding.PCM_SIGNED, 8000f, 16, 1
+//                , 16, 8000f, true);
+    }
 
     public static void main(String[] args) throws Exception {
 //        DataLine.Info info = new DataLine.Info(TargetDataLine.class, audio);
         targetDataLine = AudioSystem.getTargetDataLine(audio);
         targetDataLine.open(audio);
         targetDataLine.start();
+        if (isPlay) {
+            sourceDataLine = AudioSystem.getSourceDataLine(audio);
+            sourceDataLine.open(audio);
+            sourceDataLine.start();
+        }
         new Thread(new AudioStart()).start();
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
@@ -34,9 +45,13 @@ public class AudioStart implements Runnable {
     @Override
     public void run() {
         try {
+            byte[] temp = new byte[10240];
             while (status) {
-                byte[] temp = new byte[1024];
                 int cnt = targetDataLine.read(temp, 0, temp.length);
+                if (isPlay) {
+                    System.out.println(cnt);
+                    sourceDataLine.write(temp, 0, cnt);
+                }
                 byteArrayOutputStream.write(temp, 0, cnt);
                 byteArrayOutputStream.flush();
             }
